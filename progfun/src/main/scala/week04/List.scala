43548c885d683260579e86edf69caff61ec2b642
PiPi <qiniz@student.unimelb.edu.au>
2020-07-11 19:23:19 +0800

    add lesson 1

--- progfun/src/main/scala/week04/List.scala
+++ progfun/src/main/scala/week04/List.scala
@@ -15,3 +15,10 @@ class Nil[T] extends List[T] {
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
+
+object List {
+  // List (1, 2) = List.apply(1, 2)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T]() = new Nil
+
+}
